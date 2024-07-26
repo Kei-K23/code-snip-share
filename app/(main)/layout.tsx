1a8d8ff7e6e92c346daeea26a616f10f64adf8d1
Kei-K23 <keiksl2301@gmail.com>
2024-07-26 21:24:51 +0630

    Add main layout

---
+++ app/(main)/layout.tsx
@@ -0,0 +1,6 @@
+import { MainLayout } from "@/components/layouts/main-layout";
+import React from "react";
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return <MainLayout>{children}</MainLayout>;
+}
