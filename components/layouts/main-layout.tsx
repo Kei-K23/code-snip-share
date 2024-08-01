d36136f73cec909183eb1336f958888829ef43e7
Kei-K23 <keiksl2301@gmail.com>
2024-08-01 18:29:59 +0630

    Update landing page image

--- components/layouts/main-layout.tsx
+++ components/layouts/main-layout.tsx
@@ -6,7 +6,6 @@ import {
   Loader2,
   Menu,
   PlusCircle,
-  Search,
 } from "lucide-react";
 
 import { FaCode, FaHeart, FaTrash } from "react-icons/fa6";
@@ -148,18 +147,7 @@ export function MainLayout({ children }: MainLayoutProps) {
               </div>
             </SheetContent>
           </Sheet>
-          <div className="w-full flex-1 sticky top-0">
-            <form>
-              <div className="relative">
-                <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
-                <Input
-                  type="search"
-                  placeholder="Search..."
-                  className="w-full appearance-none bg-background pl-8 shadow-none md:w-2/3 lg:w-1/3"
-                />
-              </div>
-            </form>
-          </div>
+          <div className="w-full flex-1 sticky top-0"></div>
           <Button
             variant={"outline"}
             size={"sm"}
